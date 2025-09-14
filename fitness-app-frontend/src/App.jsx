2da05f5fa64bffed650097e3beb6c587cb2c23db
Rakshit Dhoundiyal <151960863+SlicRik8@users.noreply.github.com>
2025-09-14 14:38:17 +0530

    frontend added

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,34 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Box, Typography } from "@mui/material"
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "react-oauth2-code-pkce"
+import { useDispatch } from "react-redux";
+import {BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router"
+import { setCredentials } from "./store/authSlice";
+import ActivityDetail from "./components/ActivityDetail";
+import ActivityForm from "./components/ActivityForm";
+import ActivityList from "./components/ActivityList";
 
-function App() {
-  const [count, setCount] = useState(0)
+const ActivitiesPage = () => {
+  return (<Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+        <ActivityForm onActivitiesAdded = {() => window.location.reload()}/>
+        <ActivityList />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+       </Box>)
+  
+}
+
+function App(){
+  const { token, tokenData, logIn, logOut,isAuthenticated } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+  
+
+  useEffect(() => {
+    if(token){
+      dispatch(setCredentials({token, user: tokenData}));
+      setAuthReady(true);
+    }
+  },[token,tokenData,dispatch])
+
+
+  return(
+    <Router>
+      {!token ?(
+        <Box
+        sx={{
+          height:"100vh",
+          display:"flex",
+          flexDirection:"column",
+          alignItems:"center",
+          justifyContent: "center",
+          textAlign:"center",
+        }}>
+          <Typography variant="h4" gutterBottom>
+            Welcome to the Fitness Tracker App
+          </Typography>
+          <Typography variant="subtitle1" sx={{mb:3}}>
+            Please login to access your activities
+          </Typography>
+          <Button variant="contained" color="primary" onClick={() => {
+                      logIn(); 
+                    }}>
+            Login
+          </Button>
+        </Box>
+              ): (
+    
+      //  <div>
+      //    <pre>{JSON.stringify(tokenData,null,2)}</pre>
+      //    <pre>{JSON.stringify(token,null,2)}</pre>
+      //  </div>
+      
+       
+
+       <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+        <Button variant="contained" color="primary" onClick={logOut} sx={{
+      position: 'absolute',
+      top: 16,
+      right: 16,
+    }}>
+        Logout
+       </Button>
+        <Routes>
+          <Route path= "/activities" element={<ActivitiesPage />}/>
+          <Route path= "/activities/:id" element={<ActivityDetail />}/>
+
+          <Route path="/" element={token ? <Navigate to="/activities" replace />: <div>Welcome Please Login</div>} />
+        </Routes>
+       </Box>
+
+
+    )}
+
+    </Router>
   )
 }
 
